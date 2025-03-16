9ec31683a781a741f6590841615a69f90b29b382
eben.xie <xiegang@rockx.com>
2025-03-16 11:28:38 +0800

    [Describe]: add veBR airdrop feature

---
+++ contracts/src/interface/IVotingEscrowIncreasing.sol
@@ -0,0 +1,11 @@
+/// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface IVotingEscrowCore 
+{
+    /// @notice Deposit `_value` tokens for `_to`
+    /// @param _value Amount to deposit
+    /// @param _to Address to deposit
+    /// @return TokenId of created veNFT
+    function createLockFor(uint256 _value, address _to) external returns (uint256);
+}
